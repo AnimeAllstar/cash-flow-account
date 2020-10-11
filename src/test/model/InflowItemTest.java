4c72b27d0cdd5d850354e0fce2a3ae311fced8e8
ASad Dhorajiwala <ashcarter.awesome@gmail.com>
2020-10-11 15:22:48 +0530

    Added tests for InflowItem class

--- src/test/model/InflowItemTest.java
+++ src/test/model/InflowItemTest.java
@@ -1,5 +1,56 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// Unit tests for InflowItem class
 class InflowItemTest {
-    // delete or rename this class!
+
+    private static final LocalDate DATE = LocalDate.of(2020, Month.OCTOBER, 1);
+    private static final double AMOUNT = 150.65;
+    private ListItem listItem;
+
+    @BeforeEach
+    public void runBefore() {
+        listItem = new InflowItem(AMOUNT, DATE);
+    }
+
+    @Test
+    public void testInit() {
+        assertEquals(AMOUNT, listItem.getAmount());
+        assertEquals(DATE, listItem.getDate());
+        assertEquals("uncategorized", listItem.getCategory());
+    }
+
+
+    @Test
+    public void testCompareTo() {
+        ListItem tempItem = new InflowItem(AMOUNT, DATE);
+        assertEquals(0, listItem.compareTo(tempItem));
+
+        tempItem.setAmount(AMOUNT + 100);
+        assertEquals(AMOUNT + 100, tempItem.getAmount());
+        assertEquals(-1, listItem.compareTo(tempItem));
+
+        tempItem.setAmount(AMOUNT - 100);
+        assertEquals(AMOUNT - 100, tempItem.getAmount());
+        assertEquals(1, listItem.compareTo(tempItem));
+
+        tempItem.setDate(LocalDate.now());
+        assertEquals(LocalDate.now(), tempItem.getDate());
+        assertEquals(listItem.getDate().compareTo(tempItem.getDate()), listItem.compareTo(tempItem));
+    }
+
+    @Test
+    public void testSetCategories() {
+        assertEquals("uncategorized", listItem.getCategory());
+        assertFalse(listItem.setCategory("Invalid", InflowItem.categories));
+        assertTrue(listItem.setCategory("shopping", InflowItem.categories));
+    }
+
 }
