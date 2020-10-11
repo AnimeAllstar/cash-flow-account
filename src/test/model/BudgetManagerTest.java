c69ae568da30408927cdac2534d917466e3ddf0a
ASad Dhorajiwala <ashcarter.awesome@gmail.com>
2020-10-11 09:01:47 +0530

    Added test files

---
+++ src/test/model/BudgetManagerTest.java
@@ -0,0 +1,5 @@
+package model;
+
+class BudgetManagerTest {
+    // delete or rename this class!
+}
--- src/test/model/InflowItemTest.java
+++ src/test/model/InflowItemTest.java
@@ -1,5 +1,5 @@
 package model;
 
-public class MyModel {
+class InflowItemTest {
     // delete or rename this class!
 }
